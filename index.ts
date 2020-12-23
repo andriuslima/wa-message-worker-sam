f947e4496267e0e10ef5837d98c1b75461c55c20
Andrius Lima <andriuslima@icloud.com>
2020-12-23 17:30:17 -0300

    fix axios data when sending http request

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {Handler, SQSEvent, SQSRecord} from 'aws-lambda';
 import axios, {AxiosResponse} from 'axios';
+import qs from 'qs';
 
 const http = axios.create({baseURL: process.env.UCHAT_URL || 'https:localhost:1234'});
 const uChatToken: String = process.env.UCHAT_TOKEN || 'no-token';
@@ -16,19 +17,20 @@ const sendMessage = async (message: any) => {
     console.log(`SQS Message received: ${message}`)
     message = JSON.parse(message)
 
-    let config = {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        params: {
+    const config = {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }
+
+    const data = qs.stringify({
             'token': uChatToken,
             'cmd': 'chat',
             'to': message.phone + '@c.us',
             'msg': msg
-        }
-    }
+        })
 
     console.log(`Sending message to ${message.phone} with content \"${msg}\"`)
 
-    http.post("/", null, config).then((response: AxiosResponse) => {
+    http.post(`/${uChatToken}`, data, config).then((response: AxiosResponse) => {
         if (response.status !== 200) {
             throw Error(response.statusText)
         }
