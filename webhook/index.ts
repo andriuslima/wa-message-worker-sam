9f344b38cc93496a0271e9d1444ac3c45868901e
Andrius Lima <andriuslima@icloud.com>
2020-12-24 11:22:00 -0300

    Returning callback on webhook

--- webhook/index.ts
+++ webhook/index.ts
@@ -1,17 +1,20 @@
-import {Handler, APIGatewayEvent} from 'aws-lambda';
+import {Handler, APIGatewayEvent, Context, Callback} from 'aws-lambda';
 
-export const handler: Handler = (event: APIGatewayEvent) => {
+export const handler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
     console.log(`Path: ${event.path}`)
     console.log(`Path Param: ${event.pathParameters}`)
     console.log(`Path Body: ${event.body}`)
 
-    return {
+
+    let response =  {
         statusCode: 200,
         headers: {
             'x-custom-header' : `my custom header value`
         },
         body: JSON.stringify(`Hello World!`)
-    };
+    }
+
+    return callback(null, response)
 }
 
 
