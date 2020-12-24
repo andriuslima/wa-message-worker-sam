9a6922bcb28f9873470cac462b52feffa224c2bc
Andrius Lima <andriuslima@icloud.com>
2020-12-24 11:14:12 -0300

    Refactor project to add webhook

--- webhook/index.ts
+++ webhook/index.ts
@@ -4,6 +4,14 @@ export const handler: Handler = (event: APIGatewayEvent) => {
     console.log(`Path: ${event.path}`)
     console.log(`Path Param: ${event.pathParameters}`)
     console.log(`Path Body: ${event.body}`)
+
+    return {
+        statusCode: 200,
+        headers: {
+            'x-custom-header' : `my custom header value`
+        },
+        body: JSON.stringify(`Hello World!`)
+    };
 }
 
 
