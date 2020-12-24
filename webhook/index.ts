fed241079eee68945429547a8dbe8a069a937c5f
Andrius Lima <andriuslima@icloud.com>
2020-12-24 13:43:35 -0300

    fix get aws ssm

--- webhook/index.ts
+++ webhook/index.ts
@@ -1,28 +1,38 @@
 import {Handler, APIGatewayEvent, Context, Callback} from 'aws-lambda';
+import { SQS } from 'aws-sqs'
 import {AWSError, SSM } from 'aws-sdk'
 import { GetParameterResult } from 'aws-sdk/clients/ssm';
 import { PromiseResult } from 'aws-sdk/lib/request';
 
 const ssm = new SSM()
+const sqs = new SQS()
+const queueUrl = process.env.QUEUE
 
 export const handler: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
     const messageKey = event.pathParameters?.key || "fallback"
-    console.log(`Data: ${messageKey}`)
-
+    const body = JSON.parse(event.body);
+    const phone =  body.phone
     let message;
+
     await ssm.getParameter({ Name: messageKey, WithDecryption: false })
         .promise()
         .then((data: PromiseResult<GetParameterResult, AWSError>) => {
             console.log("On ssm promise")
-            console.log(`Data: ${data}`)
-            console.log(`Parameter: ${data.Parameter}`)
-
             message = data.Parameter?.Value
         });
 
     console.log(`Message: ${message}`)
 
-    console.log("Returning")
+    const queueMessage = JSON.stringify({message, phone})
+    const params = {
+        MessageBody: queueMessage,
+        QueueUrl: queueUrl
+    }
+
+    sqs.sendMessage(params, (err, data) => {
+        if (err) console.log(err, err.stack);
+        else     console.log(data);
+    });
 
     let response =  {
         statusCode: 200,
