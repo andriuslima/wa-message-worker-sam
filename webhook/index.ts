0141eb7d2281ffa2a06c20c5111f0911e719d340
Andrius Lima <andriuslima@icloud.com>
2021-01-06 19:07:41 -0300

    update webhook endpoint to use with active campaign API

--- webhook/index.ts
+++ webhook/index.ts
@@ -3,6 +3,8 @@ import { AWSError, SSM, SQS } from 'aws-sdk'
 import { SendMessageRequest } from 'aws-sdk/clients/sqs';
 import { GetParameterResult } from 'aws-sdk/clients/ssm';
 import { PromiseResult } from 'aws-sdk/lib/request';
+import qs from 'qs';
+
 
 const ssm = new SSM()
 const sqs = new SQS()
@@ -10,10 +12,13 @@ const queueUrl = process.env.QUEUE || "localhost"
 
 export const handler: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
     const messageKey = event.pathParameters?.key || "fallback"
-    const body = JSON.parse(event.body || "");
-    const phone =  body.phone
+    const { contact }: any = qs.parse(event.body || "")
+    const { id, phone, first_name, last_name, fields } = contact
+    const boleto = fields.link_do_boleto
+    const name = first_name + " " + last_name
     let message;
 
+    console.log(`active campaign event received for contact: ${id}:${name}:${phone}`)
     console.log(`Message key received: ${messageKey}`)
 
     await ssm.getParameter({ Name: messageKey, WithDecryption: false })
@@ -29,7 +34,7 @@ export const handler: Handler = async (event: APIGatewayEvent, context: Context,
 
     console.log(`Message retrieved: ${message}`)
 
-    const queueMessage = JSON.stringify({message, phone})
+    const queueMessage = JSON.stringify({id, phone, name, boleto, message})
     const params: SendMessageRequest = {
         MessageBody: queueMessage,
         QueueUrl: queueUrl
