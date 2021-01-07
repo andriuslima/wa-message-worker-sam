d985825a5028804a46c006a15de2f41335e514a9
Andrius Lima <andriuslima@icloud.com>
2021-01-06 22:23:45 -0300

    Try to fix problem when creation s3 bucket

--- .github/utils/create-bucket.py
+++ .github/utils/create-bucket.py
@@ -6,10 +6,9 @@ bucket = sys.argv[1]
 ACCESS_KEY = sys.argv[2]
 SECRET_KEY = sys.argv[3]
 
-s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY,)
+s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 try:
     s3.meta.client.head_bucket(Bucket=bucket)
 except ClientError:
-    # The bucket does not exist or you have no access.
     s3.create_bucket(Bucket=bucket)
