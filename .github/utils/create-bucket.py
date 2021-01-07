d65a014a00bd20b87dfb89c2a855383faa92737b
Andrius Lima <andriuslima@icloud.com>
2021-01-06 22:38:33 -0300

    Try to fix problem when creation s3 bucket

--- .github/utils/create-bucket.py
+++ .github/utils/create-bucket.py
@@ -2,13 +2,19 @@ from botocore.client import ClientError
 import boto3
 import sys
 
-bucket = sys.argv[1]
-ACCESS_KEY = sys.argv[2]
-SECRET_KEY = sys.argv[3]
+bucket = sys.argv[1].strip()
+ACCESS_KEY = sys.argv[2].strip()
+SECRET_KEY = sys.argv[3].strip()
+REGION = sys.argv[4].strip()
 
-s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+s3 = boto3.resource('s3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    region_name=REGION)
 
 try:
     s3.meta.client.head_bucket(Bucket=bucket)
 except ClientError:
-    s3.create_bucket(Bucket=bucket)
+    s3.create_bucket(
+        Bucket=bucket,
+        CreateBucketConfiguration={'LocationConstraint': REGION})
