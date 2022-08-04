35d1884ccbfc78799c2c215e1309a02f656f2672
Andrius Lima <andriuslima@icloud.com>
2022-08-03 22:32:49 -0300

    Fix formatting for phone numbers without `9`

--- lambdas/active-campaign-webhook/src/phoneFormatter.ts
+++ lambdas/active-campaign-webhook/src/phoneFormatter.ts
@@ -2,12 +2,25 @@ import parsePhoneNumber from 'libphonenumber-js';
 
 export class PhoneFormatter {
   format(phone: string): string {
-    const formattedPhone = phone.length === 8 ? '9' + phone : phone;
+    let formattedPhone = phone;
+
+    // Phone is BR and does not have an 9 in front.
+    if (phone.length === 8) {
+      formattedPhone = '9' + phone;
+    }
+
+    // Phone is BR and has a DDD without a 9.
+    if (phone.length === 10) {
+      formattedPhone = phone.substring(0, 2) + '9' + phone.substring(2);
+    }
+
     const phoneNumber = parsePhoneNumber(formattedPhone, 'BR')?.format('E.164').replace('+', '');
 
     if (!phoneNumber) {
       throw Error('Parse phone number resulted in undefined string');
     }
+
+    console.log(`Phone formatted from ${phone} to ${formattedPhone}`);
 
     return phoneNumber;
   }
